c0e82ff0c13dc0754fa28e00ebdd343d8a69df89
fuji-398 <158817600+fuji-398@users.noreply.github.com>
2024-02-10 21:41:05 +0900

    Initial add

---
+++ tab.js
@@ -0,0 +1,52 @@
+(()=>{
+
+ const $doc = document;
+ const $tab = $doc.getElementById('js-tab');
+ const $nav = $tab.querySelectorAll('[data-nav]');
+ const $content = $tab.querySelectorAll('[data-content]');
+ const ACTIVE_CLASS = 'is-active';
+ const navLen = $nav.length;
+
+ //初期化
+ const init = () => {
+  $content[0].style.display = 'block';
+ };
+ init();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    //クリックされたらnavとそのdataを取得
+    console.log('e!',e);
+
+    const $this = e.target;
+    const targetVal = $this.dataset.nav;
+
+    console.log('targetVal',targetVal);
+
+    //対象外のnav,content全て一旦リセットする
+    let index = 0;
+    while(index < navLen){
+      $content[index].style.display = 'none';
+      $nav[index].classList.remove(ACTIVE_CLASS);
+      index++;
+    };
+
+    //対象のコンテンツをアクティブ化する
+    $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+    $nav[targetVal].classList.add(ACTIVE_CLASS);
+
+  };
+
+  //全nav要素に対して関数を適応・発火
+  let index = 0;
+  while(index < navLen){
+    $nav[index].addEventListener('click',(e) => handleClick(e));
+    index++;
+  }
+
+  $nav[0].addEventListener('click',(e) => handleClick(e));
+
+})();
+
+
